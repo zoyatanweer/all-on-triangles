22cd6f7172e47ef2201424089e9873afc61c4a29
Zoya Tanweer <87615284+zoyatanweer@users.noreply.github.com>
2021-09-24 23:16:45 +0530

    updated isTriangle.js

--- isTriangle.js
+++ isTriangle.js
@@ -14,20 +14,17 @@ function isTriangle(){
         Number(inputs[1].value),
         Number(inputs[2].value) 
     ) 
-    if(sumOfAngles!=""){
-        if( (Number(inputs[0].value) ) && (Number(inputs[1].value) )===90 ||
-            (Number(inputs[1].value) ) && (Number(inputs[2].value))===90 ||
-            (Number(inputs[2].value) ) && (Number(inputs[0].value)) ===90 ){
-            outputEl.innerText = "The angles don't form a triangle."
-        }
-        else if(sumOfAngles===180){
+    if(sumOfAngles!=""){ 
+        if(sumOfAngles===180 && inputs[0].value>0 && inputs[1].value>0 && inputs[2].value>0){
             outputEl.innerText = "The angles form a triangle !"
-        }else{
-            outputEl.innerText = "Sorry, the angles don't form a triangle."
-        }   
-    }else{
+            }
+            else {
+            outputEl.innerText = "The angles dont form a triangle"
+            }
+    } else{
         outputEl.innerText ="Please enter all the values";
-    }
-}
+    }  
+} 
+
 
 isTriangleBtn.addEventListener("click", isTriangle);
